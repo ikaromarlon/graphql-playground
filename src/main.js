fd62bd9c970295fd60c5206bad5695759a6c2421
Icaro Silva <ikaromarlon@gmail.com>
2024-02-12 15:40:20 +0100

    feat: setup application server

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-function main () {
-  console.log('hello world')
+import { getServer } from './server.js'
+
+async function main () {
+  try {
+    const server = getServer()
+
+    await server.start()
+
+    console.log('App is running at', server.getAddress())
+  } catch (e) {
+    console.log('App failed to start...', e)
+  }
 }
 
-main()
+await main()
---
+++ src/server.js
@@ -0,0 +1,47 @@
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+const state = {
+  instance: null,
+  isConnected: false,
+  address: null
+}
+
+export function getServer () {
+  if (state.instance === null) {
+    state.instance = new ApolloServer({
+      // typeDefs,
+      // resolvers,
+    })
+  }
+
+  const start = async () => {
+    const { url } = await startStandaloneServer(state.instance, {
+      listen: { port: 3000 }
+    })
+    state.address = url
+    state.isConnected = true
+  }
+
+  const isConnected = () => {
+    return state.isConnected
+  }
+
+  const getAddress = () => {
+    if (!isConnected()) throw new Error('Server is not connected')
+    return state.address
+  }
+
+  const shutDown = async () => {
+    await state.instance.stop()
+    state.isConnected = false
+    state.address = null
+  }
+
+  return {
+    start,
+    isConnected,
+    getAddress,
+    shutDown
+  }
+}
