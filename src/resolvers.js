7781d391425dea83910082510b64a35332bc60ae
Icaro Silva <ikaromarlon@gmail.com>
2024-02-12 22:47:37 +0100

    feat: add mutations for Game schema

--- src/resolvers.js
+++ src/resolvers.js
@@ -38,6 +38,29 @@ const resolvers = {
     game (parent) {
       return db.games.find(game => game.id === parent.gameId)
     }
+  },
+  Mutation: {
+    addGame (_, args) {
+      const game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 1000).toString()
+      }
+      db.games.push(game)
+      return game
+    },
+    updateGame (_, args) {
+      db.games = db.games.map(game => {
+        if (game.id === args.id) {
+          return { ...game, ...args.edits }
+        }
+        return game
+      })
+      return db.games.find(game => game.id === args.id)
+    },
+    deleteGame (_, args) {
+      db.games = db.games.filter(game => game.id !== args.id)
+      return true
+    }
   }
 }
 
--- src/schema.js
+++ src/schema.js
@@ -26,4 +26,17 @@ export const typeDefs = `#graphql
     authors: [Author]
     author(id: ID!): Author
   }
+  type Mutation {
+    addGame(game: AddGameInput!): Game
+    updateGame(id: ID!, edits: EditGameInput): Game
+    deleteGame(id: ID!): Boolean
+  }
+  input AddGameInput {
+    title: String!
+    platform: [String!]!
+  }
+  input EditGameInput {
+    title: String
+    platform: [String!]
+  }
 `
