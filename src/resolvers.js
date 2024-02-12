5e4e25e6f2494e610ede04a5ad5ca3858bc6a82b
Icaro Silva <ikaromarlon@gmail.com>
2024-02-12 21:49:18 +0100

    feat: add resolvers for relationship among resources

--- src/resolvers.js
+++ src/resolvers.js
@@ -20,6 +20,24 @@ const resolvers = {
     review (_, args) {
       return db.reviews.find(review => review.id === args.id)
     }
+  },
+  Game: {
+    reviews (parent) {
+      return db.reviews.filter(review => review.gameId === parent.id)
+    }
+  },
+  Author: {
+    reviews (parent) {
+      return db.reviews.filter(review => review.authorId === parent.id)
+    }
+  },
+  Review: {
+    author (parent) {
+      return db.authors.find(author => author.id === parent.authorId)
+    },
+    game (parent) {
+      return db.games.find(game => game.id === parent.gameId)
+    }
   }
 }
 
